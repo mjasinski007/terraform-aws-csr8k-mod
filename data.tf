f01dffa80904fae0ddc301bfa604e60293a4eec6
vMario <mjasinski@aviatrix.com>
2023-11-22 15:09:26 +0100

    Fix Cisco CSR AMI

---
+++ data.tf
@@ -0,0 +1,29 @@
+data "aws_region" "current" {}
+
+# Retrieve my public IP address
+data "http" "my_public_ip" {
+    url = "http://ipv4.icanhazip.com"
+}
+
+# CSR AMI
+data "aws_ami" "this" {
+    owners      = ["aws-marketplace"]
+    #owners      = ["679593333241"] # Cisco Systems
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = var.csr_ami == "BYOL" ? [var.csr_ami_byol_ami] : [var.csr_ami_sec_ami]
+        #values = var.prioritize == "price" ? ["cisco_CSR-17.03.06-BYOL-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3ami-0d8ad992c259060ef"] : ["cisco_CSR-.17.3.3-SEC-dbfcb230-402e-49cc-857f-dacb4db08d34"]
+        #values = ["cisco_CSR-.16.12.06-BYOL-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3"]
+    }
+}
+
+data "template_file" "running_config" {
+    template = file("${path.module}/running-config.tpl")
+
+    vars = {
+        admin_password = var.admin_password
+        hostname       = var.csr_hostname
+    }
+}
