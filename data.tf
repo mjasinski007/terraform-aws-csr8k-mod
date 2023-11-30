1ee129e4df236de3daced3f778ee563a0fd6e139
vMario-Aviatrix <102957943+mjasinski007@users.noreply.github.com>
2023-11-30 13:58:00 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -7,15 +7,15 @@ data "http" "my_public_ip" {
 
 # CSR AMI
 data "aws_ami" "this" {
-    #owners      = ["aws-marketplace"]
-    owners      = ["679593333241"] # Cisco Systems
+    owners      = ["aws-marketplace"]
+    #owners      = ["679593333241"] # Cisco Systems
     most_recent = true
 
     filter {
         name   = "name"
-        values = var.csr_ami == "BYOL" ? [var.csr_ami_byol_ami] : [var.csr_ami_sec_ami]
+        #values = var.csr_ami == "BYOL" ? [var.csr_ami_byol_ami] : [var.csr_ami_sec_ami]
         #values = var.prioritize == "price" ? ["cisco_CSR-17.03.06-BYOL-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3ami-0d8ad992c259060ef"] : ["cisco_CSR-.17.3.3-SEC-dbfcb230-402e-49cc-857f-dacb4db08d34"]
-        #values = ["cisco_CSR-.16.12.06-BYOL-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3"]
+        values = ["Cisco-C8K-17.07.01a-42cb6e93-8d9d-490b-a73c-e3e56077ffd1"]
     }
 }
 
